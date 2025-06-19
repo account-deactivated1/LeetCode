ce6d096ef997123f679048479af1fd8e2de3b7eb
Shafiq Ur Rehman <Shafiqurrehmanbscs2022@gmail.com>
2025-06-19 20:55:18 +0500

    Time: 4 ms (53.41%) | Memory: 24.3 MB (45.62%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9] = {0}, columns[9] = {0}, boxes[9] = {0};
         for (int i = 0; i < 9; ++i) {
-            for (int j = 0; j < 9; ++j) {
-                if (board[i][j] == '.')
-                    continue;
-                int num = board[i][j] - '1';
-                int mask = 1 << num;
-                int boxIndex = (i / 3) * 3 + j / 3;
+            unordered_set<char> row;
+            unordered_set<char> col;
+            unordered_set<char> box;
 
-                if (rows[i] & mask || columns[j] & mask ||
-                    boxes[boxIndex] & mask) {
-                    return false;
+            for (int j = 0; j < 9; ++j) {
+                if (board[i][j] != '.') {
+                    if (row.count(board[i][j]))
+                        return false;
+                    row.insert(board[i][j]);
                 }
-                rows[i] |= mask;
-                columns[j] |= mask;
-                boxes[boxIndex] |= mask;
+                if (board[j][i] != '.') {
+                    if (col.count(board[j][i]))
+                        return false;
+                    col.insert(board[j][i]);
+                }
+                int boxRow = 3 * (i / 3) + j / 3;
+                int boxCol = 3 * (i % 3) + j % 3;
+                if (board[boxRow][boxCol] != '.') {
+                    if (box.count(board[boxRow][boxCol]))
+                        return false;
+                    box.insert(board[boxRow][boxCol]);
+                }
             }
         }
         return true;

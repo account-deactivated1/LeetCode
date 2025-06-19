e8040f66db766253c141b01e8ec9e5ec11486701
Shafiq Ur Rehman <Shafiqurrehmanbscs2022@gmail.com>
2025-06-19 18:21:33 +0500

    Time: 11 ms (92.38%) | Memory: 24.9 MB (84.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagrams;
-        for (string& s : strs) {
+        for (string s : strs) {
             string key = s;
             sort(key.begin(), key.end());
             anagrams[key].push_back(s);

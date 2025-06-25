19abc8a72ca66e198e45599d51a8443c0ed35f0c
Shafiq Ur Rehman <Shafiqurrehmanbscs2022@gmail.com>
2025-06-25 15:29:36 +0500

    Time: 1 ms (61.43%) | Memory: 9.6 MB (74.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       if(s.size()!=t.size()){
-        return false;
-       };
-       sort(s.begin() , s.end());
-       sort(t.begin(),t.end());
-       return s == t;
-    }
+        if (s.size() != t.size())  return false;
+        int count[26] = {0};
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        };
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)  return false;
+        };
+        return true;
+    };
 };

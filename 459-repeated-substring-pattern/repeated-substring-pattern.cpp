d6ba9b480e027e1bc0ddab52d5508f5c8267cb4b
Shafiq Ur Rehman <Shafiqurrehmanbscs2022@gmail.com>
2025-06-27 15:16:13 +0500

    Time: 4 ms (79.69%) | Memory: 15.1 MB (78.82%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        string t = s + s;                           
-        string trimmed = t.substr(1, t.size() - 2); 
-        return trimmed.find(s) != -1;              
+        string a = s + s;
+        string b = a.substr(1,a.size()-2);
+        return b.find(s) != -1;   // true                         
     }
 };
